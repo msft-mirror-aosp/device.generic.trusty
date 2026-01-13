3134e6485e5ed73ff46ca911192c8cb88c028647
Orlando Arbildo <oarbildo@google.com>
2026-01-13 00:38:53 +0000

    cmd_processor: Modifying SEPolicy
    Modifying SEPolicy so the vendor app and launcher can
    work.
    
    Bug: 454503409
    Test: m
    Change-Id: I92b0d5ac4390e8c0f45b29101fb7f49382194e3a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ TARGET_2ND_CPU_VARIANT := generic
 
 BOARD_SEPOLICY_DIRS += device/generic/trusty/sepolicy
 
+BOARD_SEPOLICY_DIRS += device/generic/trusty/custom_vm/sepolicy
 
 # We want goldfish build configuration information, but not the resulting
 # QEMU images. QEMU_CUSTOMIZATIONS turns this on without building the images
